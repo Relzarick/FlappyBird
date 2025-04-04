f6fda26c547f82930b058a921b1c80fa97bfbfe5
Exodus <junfeng.nyo@gmail.com>
2025-04-04 20:42:56 +0800

    added bird child class and added animations for it (too fast)

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ class SpriteLoader {
 
     this.isLoaded = false;
     this.isLogged = false;
+
+    this.width = this.img.width;
+    this.height = this.img.height;
   }
 
   async load() {
@@ -19,7 +22,7 @@ class SpriteLoader {
 
   //
 
-  draw(ctx, x, y, width = this.img.width, height = this.img.height) {
+  draw(ctx, x, y, width = this.width, height = this.height) {
     // * Only draws once image is loaded
     if (this.isLoaded) {
       ctx.drawImage(this.img, x, y, width, height);
@@ -30,17 +33,53 @@ class SpriteLoader {
   }
 }
 
-// t
+class birdSprite extends SpriteLoader {
+  constructor(source) {
+    super(source);
+
+    this.spriteWidth = 92;
+    this.sourceX = 0;
+
+    this.width = 92;
+    this.height = 64;
+
+    this.totalSprite = 3;
+    this.currentSprite = 0;
+  }
+
+  updateFrameIndex() {
+    this.sourceX = this.currentSprite * this.spriteWidth; // t Gets the starting point of each sprite
+    this.currentSprite = (this.currentSprite + 1) % this.totalSprite;
+  }
+
+  renderAnimations(ctx, x, y) {
+    if (this.isLoaded) {
+      ctx.drawImage(
+        this.img,
+        this.sourceX,
+        0,
+        this.spriteWidth,
+        this.height,
+        x,
+        y,
+        this.width,
+        this.height
+      );
+      this.updateFrameIndex();
+    }
+  }
+}
 
 //  t
 
 const canvas = document.querySelector("#canvas");
 canvas.width = 700;
 canvas.height = 1000;
-
 const ctx = canvas.getContext("2d");
 
-const flappy = new SpriteLoader("images/bird.png");
+const flappy = new birdSprite("images/bird.png");
+// Each bird sprite is 92px wide
+
 const background = new SpriteLoader("images/background.png");
 
 let groundOffset = 0;
@@ -64,7 +103,7 @@ const assetsPreLoader = async () => {
 const gameLoop = () => {
   background.draw(ctx, 0, -128, canvas.width, canvas.height);
 
-  flappy.draw(ctx, 200, 400);
+  flappy.renderAnimations(ctx, 200, 400);
 
   // * Draw the ground sprite multiple times to cover the canvas width
   for (let x = -groundOffset; x < canvas.width; x += ground.img.width) {
@@ -78,7 +117,13 @@ const gameLoop = () => {
   requestAnimationFrame(gameLoop);
 };
 
+const gravity = () => {};
+
+//
+
 assetsPreLoader();
+
+//
 
 // ! Development Guidelines
 //? Define the core mechanics: the bird’s movement (flap, gravity, drop)
@@ -88,9 +133,6 @@ assetsPreLoader();
 
 // ? Integrate keyboard (space bar) touch events (mobile)
 // Provide immediate visual and audio feedback for events (collision, and game-over transitions)
-
-// Use OOP
-// Ensure encapsulation for maintainability.
 
 // t Set up an HTML5 <canvas> for drawing and animation
 // Initializing the 2D rendering context for smoother graphics.
@@ -111,5 +153,3 @@ assetsPreLoader();
 //verify that score resets and state transitions are robust.
 
 //persistent high scores using local storage
-
-// use image() onload event and call drawimage()
